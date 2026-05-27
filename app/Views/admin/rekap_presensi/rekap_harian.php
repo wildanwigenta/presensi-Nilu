37767bea763d083bd68e7d6eb697b3a619258051
Wildan <wildanwigenta52@gmail.com>
2026-05-28 00:02:17 +0700

    feat: clear admin/rekpres/rekbul

--- app/Views/admin/rekap_presensi/rekap_harian.php
+++ app/Views/admin/rekap_presensi/rekap_harian.php
@@ -16,9 +16,9 @@
 
 <span>Menampilkan Data :
     <?php if ($tanggal) : ?>
-        <?= date('d F Y', strtotime($tanggal)) ?>
+    <?= date('d F Y', strtotime($tanggal)) ?>
     <?php else : ?>
-        <?= date('d F Y') ?>
+    <?= date('d F Y') ?>
     <?php endif; ?>
 </span>
 
@@ -38,8 +38,8 @@
         </thead>
         <tbody>
             <?php if (!empty($rekap_harian)) : ?>
-                <?php $no = 1; foreach ($rekap_harian as $rekap) : ?>
-                    <?php
+            <?php $no = 1; foreach ($rekap_harian as $rekap) : ?>
+            <?php
                     // Hitung total jam kerja
                     $total_jam_kerja = '-';
                     if (!empty($rekap['jam_masuk']) && !empty($rekap['jam_keluar']) 
@@ -70,21 +70,21 @@
                         }
                     }
                     ?>
-                    <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?= esc($rekap['nama']) ?></td>
-                        <td><?= esc($rekap['nama_shift'] ?? '-') ?></td>
-                        <td><?= date('d F Y', strtotime($rekap['tanggal_masuk'])) ?></td>
-                        <td><?= esc($rekap['jam_masuk']) ?></td>
-                        <td><?= !empty($rekap['jam_keluar']) ? esc($rekap['jam_keluar']) : '-' ?></td>
-                        <td><?= $total_jam_kerja ?></td>
-                        <td><?= $total_terlambat ?></td>
-                    </tr>
-                <?php endforeach; ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td><?= esc($rekap['nama']) ?></td>
+                <td><?= esc($rekap['nama_shift'] ?? '-') ?></td>
+                <td><?= date('d F Y', strtotime($rekap['tanggal_masuk'])) ?></td>
+                <td><?= esc($rekap['jam_masuk']) ?></td>
+                <td><?= !empty($rekap['jam_keluar']) ? esc($rekap['jam_keluar']) : '-' ?></td>
+                <td><?= $total_jam_kerja ?></td>
+                <td><?= $total_terlambat ?></td>
+            </tr>
+            <?php endforeach; ?>
             <?php else : ?>
-                <tr>
-                    <td colspan="8" class="text-center">Tidak ada data presensi</td>
-                </tr>
+            <tr>
+                <td colspan="8" class="text-center">Tidak ada data presensi</td>
+            </tr>
             <?php endif; ?>
         </tbody>
     </table>
