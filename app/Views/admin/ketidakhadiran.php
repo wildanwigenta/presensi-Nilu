15df80c798828394a23359ad19a1a09d1cc204f0
Wildan <wildanwigenta52@gmail.com>
2026-06-04 13:55:29 +0700

    feat: clear admin/ketidakhadiran

--- app/Views/admin/ketidakhadiran.php
+++ app/Views/admin/ketidakhadiran.php
@@ -16,32 +16,32 @@
     </thead>
     <tbody>
         <?php if ($ketidakhadiran) : ?>
-            <?php $no = 1; ?>
-            <?php foreach ($ketidakhadiran as $item) : ?>
-                <tr>
-                    <td><?= $no++ ?></td>
-                    <td><?= $item['tanggal'] ?></td>
-                    <td><?= $item['keterangan'] ?></td>
-                    <td><?= $item['deskripsi'] ?></td>
-                    <td>
-                        <a class="badge bg-primary" href="<?= base_url('file_ketidakhadiran/' . $item['file']) ?>">
-                            Download
-                        </a>
-                    </td>
-                    <td>
-                        <?php if ($item['status'] == 'Pending') : ?>
-                            <span class="text-danger"><?= $item['status'] ?></span>
-                        <?php else : ?>
-                            <span class="text-success"><?= $item['status'] ?></span>
-                        <?php endif; ?>
-                    </td>
-                    <td>
-                        <a class="badge bg-success" href="<?= base_url('admin/approved_ketidakhadiran/' . $item['id']) ?>">
-                            Approved
-                        </a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+        <?php $no = 1; ?>
+        <?php foreach ($ketidakhadiran as $item) : ?>
+        <tr>
+            <td><?= $no++ ?></td>
+            <td><?= $item['tanggal'] ?></td>
+            <td><?= $item['keterangan'] ?></td>
+            <td><?= $item['deskripsi'] ?></td>
+            <td>
+                <a class="badge bg-primary" href="<?= base_url('file_ketidakhadiran/' . $item['file']) ?>">
+                    Download
+                </a>
+            </td>
+            <td>
+                <?php if ($item['status'] == 'Pending') : ?>
+                <span class="text-danger"><?= $item['status'] ?></span>
+                <?php else : ?>
+                <span class="text-success"><?= $item['status'] ?></span>
+                <?php endif; ?>
+            </td>
+            <td>
+                <a class="badge bg-success" href="<?= base_url('admin/approved_ketidakhadiran/' . $item['id']) ?>">
+                    Approved
+                </a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
         <?php endif; ?>
     </tbody>
 </table>
