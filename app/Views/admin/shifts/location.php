d82d607796b6c0bd13e22d790e6ac6303b38009c
Wildan <wildanwigenta52@gmail.com>
2026-06-04 13:54:41 +0700

    feat: clear admin/shift/location

--- app/Views/admin/shifts/location.php
+++ app/Views/admin/shifts/location.php
@@ -26,7 +26,8 @@
             <td><?= esc($shift['jam_keluar']) ?></td>
             <td>
                 <a href="<?= base_url('admin/shifts/edit/' . $shift['id']) ?>" class="badge bg-primary">Edit</a>
-                <a href="<?= base_url('admin/shifts/delete/' . $shift['id']) ?>" class="badge bg-danger tombol-hapus">Hapus</a>
+                <a href="<?= base_url('admin/shifts/delete/' . $shift['id']) ?>"
+                    class="badge bg-danger tombol-hapus">Hapus</a>
             </td>
         </tr>
         <?php endforeach; ?>
